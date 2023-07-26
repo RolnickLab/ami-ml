a5dc5216afb30162e85eb2c75bfc95e7249eb782
adityajain07 <aditya.jain@mila.quebec>
2023-07-26 10:54:31 -0400

    Rearrange folders

---
+++ active_learning/code/acquisition_functions.py
@@ -0,0 +1,17 @@
+"""
+Author : Aditya Jain
+Date last modified : July 19, 2023
+About : Function definitions for the different acquisition functions in active learning
+"""
+
+def least_confidence():
+    pass
+
+def margin_sampling():
+    pass
+
+def entropy():
+    pass
+
+
+
