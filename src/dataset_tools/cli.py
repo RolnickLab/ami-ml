e3a9e790a7323093f05db55088ca07afd43f4a8f
f-PLT <pelletier.f@gmail.com>
2024-04-18 14:49:24 -0400

    Init single CLI structure with fetch_images.py and clean_dataset.py

---
+++ src/dataset_tools/cli.py
@@ -0,0 +1,211 @@
+import functools
+
+import click
+
+from src.dataset_tools.clean_dataset import clean_dataset
+from src.dataset_tools.fetch_images import fetch_images
+
+CLEAN_DATASET = "clean-dataset"
+FETCH_IMAGES = "fetch-images"
+
+
+def with_dwca_file(func):
+    @functools.wraps(func)
+    @click.option(
+        "--dwca-file",
+        type=str,
+        required=True,
+        help="Darwin Core Archive file",
+    )
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+#
+# Clean Dataset
+#
+@click.command(name=CLEAN_DATASET, context_settings={"show_default": True})
+@with_dwca_file
+@click.option(
+    "--verified-data-csv",
+    type=str,
+    required=True,
+    help="CSV file containing verified image info",
+)
+@click.option(
+    "--remove-duplicate-url",
+    type=bool,
+    default=True,
+    help="Whether occurrences with duplicate URLs should be removed.",
+)
+@click.option(
+    "--remove-tumbnails",
+    type=bool,
+    default=True,
+    help=(
+        "Whether small images should be removed. Use the option --thumb_size to "
+        "determine the minimum side size."
+    ),
+)
+@click.option(
+    "--thumb-size",
+    type=int,
+    default=64,
+    help="Minimum side size to an image not be considered as thumbnail",
+)
+@click.option(
+    "--ignore-dataset-by-key",
+    type=str,
+    default=(
+        "f3130a8a-4508-42b4-9737-fbda77748438,"
+        "4bfac3ea-8763-4f4b-a71a-76a6f5f243d3,"
+        "7e380070-f762-11e1-a439-00145eb45e9a"
+    ),
+    help=(
+        "DatasetKeys separeted by comma. Some datasets might be ignored due to the "
+        "poor quality of their images."
+    ),
+)
+@click.option(
+    "--remove-non-adults",
+    type=bool,
+    default=True,
+    help="Whether keeping only occurrences with lifeStage identified as Adult or Imago",
+)
+@click.option(
+    "--life-stage-predictions",
+    type=str,
+    help=(
+        "CSV containing life-stage predictions for images. If provided images without"
+        " the GBIF life stage will be filtered based on life stage prediction"
+    ),
+)
+def clean_dataset_command(
+    dwca_file: str,
+    verified_data_csv: str,
+    remove_duplicate_url: bool,
+    ignore_dataset_by_key: str,
+    remove_tumbnails: bool,
+    thumb_size: int,
+    remove_non_adults: bool,
+    life_stage_predictions: str,
+):
+    clean_dataset(
+        dwca_file=dwca_file,
+        verified_data_csv=verified_data_csv,
+        remove_duplicate_url=remove_duplicate_url,
+        ignore_dataset_by_key=ignore_dataset_by_key,
+        remove_tumbnails=remove_tumbnails,
+        thumb_size=thumb_size,
+        remove_non_adults=remove_non_adults,
+        life_stage_predictions=life_stage_predictions,
+    )
+
+
+#
+# Fetch Images Command
+#
+@click.command(name=FETCH_IMAGES, context_settings={"show_default": True})
+@with_dwca_file
+@click.option(
+    "--dataset-path", type=str, required=True, help="Folder to save images to"
+)
+@click.option(
+    "--cache-path",
+    type=str,
+    help=(
+        "Folder containing cached images. If provided, the script will try copy"
+        " images from cache before trying fetch them."
+    ),
+)
+@click.option(
+    "--num-workers",
+    type=int,
+    default=8,
+    help="Number of processes to download in images in parallel",
+)
+@click.option(
+    "--dwca-file",
+    type=str,
+    required=True,
+    help="Darwin Core Archive file",
+)
+@click.option(
+    "--subset-list",
+    type=str,
+    help=(
+        "JSON file with the list of keys to be fetched."
+        "If provided, only occorrences with these keys will be fetched. Use the option"
+        " --subset_key to define the field to be used for filtering."
+    ),
+)
+@click.option(
+    "--subset-key",
+    type=str,
+    default="acceptedTaxonKey",
+    help=(
+        "DwC-A field to use for filtering occurrences to be fetched. "
+        "See --subset_list"
+    ),
+)
+@click.option(
+    "--num-images-per-category",
+    type=int,
+    default=0,
+    help=(
+        "Number of images to be downloaded for each category. If not provided, all "
+        "images will be fetched."
+    ),
+)
+@click.option(
+    "--request-timeout",
+    type=int,
+    default=30,
+    help=("Timout for closing urresponsive connections."),
+)
+@click.option(
+    "--random-seed",
+    type=int,
+    default=42,
+    help="Random seed for reproductible experiments",
+)
+def fetch_images_command(
+    dwca_file: str,
+    num_workers: int,
+    dataset_path: str,
+    cache_path: str,
+    subset_list: str,
+    subset_key: str,
+    num_images_per_category: int,
+    request_timeout: int,
+    random_seed: int,
+):
+    fetch_images(
+        dwca_file=dwca_file,
+        num_workers=num_workers,
+        dataset_path=dataset_path,
+        cache_path=cache_path,
+        subset_list=subset_list,
+        subset_key=subset_key,
+        num_images_per_category=num_images_per_category,
+        request_timeout=request_timeout,
+        random_seed=random_seed,
+    )
+
+
+#
+# Main CLI configuration
+#
+@click.group()
+def cli():
+    """This is the main command line interface for dataset tools."""
+    pass
+
+
+cli.add_command(clean_dataset_command)
+cli.add_command(fetch_images_command)
+
+if __name__ == "__main__":
+    cli()
--- src/dataset_tools/fetch_images.py
+++ src/dataset_tools/fetch_images.py
@@ -10,13 +10,13 @@ import shutil
 from functools import partial
 from multiprocessing import Pool
 
-import click
 import pandas as pd
 import requests
-from utils import get_image_path, load_dwca_data, set_random_seeds
+
+from src.dataset_tools.utils import get_image_path, load_dwca_data, set_random_seeds
 
 
-def get_and_verify_image_path(image_data, dataset_path: str):
+def _get_and_verify_image_path(image_data, dataset_path: str):
     image_path = os.path.join(dataset_path, get_image_path(image_data))
 
     dirs = os.path.dirname(image_path)
@@ -30,7 +30,7 @@ def get_and_verify_image_path(image_data, dataset_path: str):
     return image_path
 
 
-def try_copy_from_cache(image_path: str, dataset_path: str, cache_path: str):
+def _try_copy_from_cache(image_path: str, dataset_path: str, cache_path: str):
     if cache_path is not None:
         image_cache = os.path.relpath(image_path, dataset_path)
         image_cache = os.path.join(cache_path, image_cache)
@@ -42,7 +42,7 @@ def try_copy_from_cache(image_path: str, dataset_path: str, cache_path: str):
     return False
 
 
-def url_retrieve(url, file_path, timeout):
+def _url_retrieve(url, file_path, timeout):
     headers = {
         "User-Agent": (
             "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0) "
@@ -54,86 +54,48 @@ def url_retrieve(url, file_path, timeout):
     open(file_path, "wb").write(r.content)
 
 
-def fetch_image(image_data, dataset_path: str, cache_path: str, timeout: int):
+def _fetch_image(image_data, dataset_path: str, cache_path: str, timeout: int):
     url = image_data["identifier"]
-    image_path = get_and_verify_image_path(image_data, dataset_path)
+    image_path = _get_and_verify_image_path(image_data, dataset_path)
 
     if image_path is not None:
-        cached = try_copy_from_cache(image_path, dataset_path, cache_path)
+        cached = _try_copy_from_cache(image_path, dataset_path, cache_path)
 
         if not cached and not os.path.isfile(image_path):
             try:
-                url_retrieve(url, image_path, timeout)
+                _url_retrieve(url, image_path, timeout)
                 print(f"Image fetched from {url}")
             except Exception as e:
                 print(f"Error fetching {url}")
                 print(e)
 
 
-@click.command(context_settings={"show_default": True})
-@click.option(
-    "--dataset-path", type=str, required=True, help="Folder to save images to"
-)
-@click.option(
-    "--cache-path",
-    type=str,
-    help=(
-        "Folder containing cached images. If provided, the script will try copy"
-        " images from cache before trying fetch them."
-    ),
-)
-@click.option(
-    "--num-workers",
-    type=int,
-    default=8,
-    help="Number of processes to download in images in parallel",
-)
-@click.option(
-    "--dwca-file",
-    type=str,
-    required=True,
-    help="Darwin Core Archive file",
-)
-@click.option(
-    "--subset-list",
-    type=str,
-    help=(
-        "JSON file with the list of keys to be fetched."
-        "If provided, only occorrences with these keys will be fetched. Use the option"
-        " --subset_key to define the field to be used for filtering."
-    ),
-)
-@click.option(
-    "--subset-key",
-    type=str,
-    default="acceptedTaxonKey",
-    help=(
-        "DwC-A field to use for filtering occurrences to be fetched. "
-        "See --subset_list"
-    ),
-)
-@click.option(
-    "--num-images-per-category",
-    type=int,
-    default=0,
-    help=(
-        "Number of images to be downloaded for each category. If not provided, all "
-        "images will be fetched."
-    ),
-)
-@click.option(
-    "--request-timeout",
-    type=int,
-    default=30,
-    help=("Timout for closing urresponsive connections."),
-)
-@click.option(
-    "--random-seed",
-    type=int,
-    default=42,
-    help="Random seed for reproductible experiments",
-)
-def main(
+def _subset_list(gbif_metadata, subset_key, subset_list):
+    with open(subset_list) as f:
+        keys_list = json.load(f)
+        keys_list = [int(x) for x in keys_list]
+    gbif_metadata = gbif_metadata[gbif_metadata[subset_key].isin(keys_list)].copy()
+    return gbif_metadata
+
+
+def _group_by_category(gbif_metadata, num_images_per_category, subset_key):
+    all_occ = (
+        gbif_metadata.groupby(subset_key)
+        .filter(lambda x: len(x) < num_images_per_category)
+        .copy()
+    )
+    subsampling_occ = (
+        gbif_metadata.groupby(subset_key)
+        .filter(lambda x: len(x) >= num_images_per_category)
+        .groupby(subset_key)
+        .sample(num_images_per_category)
+        .copy()
+    )
+    gbif_metadata = pd.concat([all_occ, subsampling_occ])
+    return gbif_metadata
+
+
+def fetch_images(
     dwca_file: str,
     num_workers: int,
     dataset_path: str,
@@ -147,37 +109,20 @@ def main(
     set_random_seeds(random_seed)
     gbif_metadata = load_dwca_data(dwca_file)
     if subset_list is not None:
-        with open(subset_list) as f:
-            keys_list = json.load(f)
-            keys_list = [int(x) for x in keys_list]
-        gbif_metadata = gbif_metadata[gbif_metadata[subset_key].isin(keys_list)].copy()
+        gbif_metadata = _subset_list(gbif_metadata, subset_key, subset_list)
 
     if num_images_per_category > 0:
-        all_occ = (
-            gbif_metadata.groupby(subset_key)
-            .filter(lambda x: len(x) < num_images_per_category)
-            .copy()
+        gbif_metadata = _group_by_category(
+            gbif_metadata, num_images_per_category, subset_key
         )
-        subsampling_occ = (
-            gbif_metadata.groupby(subset_key)
-            .filter(lambda x: len(x) >= num_images_per_category)
-            .groupby(subset_key)
-            .sample(num_images_per_category)
-            .copy()
-        )
-        gbif_metadata = pd.concat([all_occ, subsampling_occ])
 
     _, list_images = zip(*gbif_metadata.iterrows())
 
     fetch_image_f = partial(
-        fetch_image,
+        _fetch_image,
         dataset_path=dataset_path,
         cache_path=cache_path,
         timeout=request_timeout,
     )
     with Pool(processes=num_workers) as pool:
         pool.map(fetch_image_f, list_images)
-
-
-if __name__ == "__main__":
-    main()
