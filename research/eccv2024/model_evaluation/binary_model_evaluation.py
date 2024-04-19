daf7b6093391b05950c53b5824346899dfb8955e
adityajain07 <aditya.jain@mila.quebec>
2024-04-19 12:15:53 -0400

    Binary model evaluation working

--- research/eccv2024/model_evaluation/binary_model_evaluation.py
+++ research/eccv2024/model_evaluation/binary_model_evaluation.py
@@ -6,9 +6,7 @@ About: Evaluation of AMI-GBIF trained binary classifier on AMI-Traps data
 
 import os
 import glob
-import webdataset as wds
-from itertools import islice
-import pandas as pd
+import json
 import argparse
 from PIL import Image
 import torch
@@ -18,31 +16,21 @@ import wandb
 from model_inference import ModelInference
 
 
-def identity(x):
-    return x
-
-
 def binary_model_evaluation(
     run_name: str,
     artifact: str,
     model_type: str,
     model_dir: str,
     category_map: str,
-    wbds_files: str,
+    insect_crops_dir: str,
+    skip_small_crops: bool,
+    min_crop_dim: int = 0
 ):
     """Main function for binary model evaluation"""
 
     # Get the environment variables
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"{device} is available.")
-
-    # Build the webdataset for binary classification dataset
-    transform_img = transforms.Compose([transforms.ToTensor()])
-    data = (wds.WebDataset(wbds_files)
-           .decode("pil")
-           .to_tuple("jpg", "json")
-           .map_tuple(transform_img, identity))
-    dataloader = torch.utils.data.DataLoader(data, num_workers=1)
+    print(f"Device {device} is available.")
 
     # Download the model
     api = wandb.Api()
@@ -55,27 +43,37 @@ def binary_model_evaluation(
     new_model = os.path.join(model_dir, run_name + ".pth")
     os.rename(latest_file, new_model)
 
-    new_model = os.path.join(model_dir, "moth-nonmoth_resnet50_20230604_065440_30.pth") # TEST
-
     # Build the binary classification model
     categ_map_path = os.path.join(model_dir, category_map)
     binary_classifier = ModelInference(new_model, model_type, categ_map_path, device)
+
+    # Get all insect crops and label information
+    insect_crops = glob.glob(os.path.join(insect_crops_dir, "*.jpg"))
+    insect_labels = json.load(open(os.path.join(insect_crops_dir, "binary_labels.json")))
 
     # Evaluation metrics variables
     tp, tn, fp, fn = 0, 0, 0, 0
     gt_moths, gt_nonmoths = 0, 0
 
     # Iterate over the files in webdataset
-    for image, annotation in dataloader: 
+    for image_path in insect_crops: 
 
-        image = image.to(
-                device, non_blocking=True
-        )
+        # Read the image
+        image = Image.open(image_path)
 
-        # Get ground truth label 
-        gt_label = annotation["label"][0]
+        # If skipping small crops, check size
+        if skip_small_crops:
+            width, height = image.width, image.height
+            if width < min_crop_dim and height < min_crop_dim:
+                continue
+
+        # Get ground truth label
+        img_name = os.path.split(image_path)[1] 
+        gt_label = insect_labels[img_name]["label"]
 
         # Binary model prediction
+        transform_to_tensor = transforms.Compose([transforms.ToTensor()])
+        image = transform_to_tensor(image)
         pred = binary_classifier.predict(image)[0][0]
 
         # Fill up evluation metrics 
@@ -88,7 +86,7 @@ def binary_model_evaluation(
             else: fp += 1
             gt_nonmoths += 1
         else:
-            raise Exception("Unknown binary label for a moth crop!")
+            raise Exception("Unknown binary label for an insect crop!")
         
     # Aggregated metrics
     total_crops = gt_moths + gt_nonmoths
@@ -99,6 +97,7 @@ def binary_model_evaluation(
 
     print(
         f"\nBinary classification evaluation for {run_name}:\
+        \nSkip small crops - {skip_small_crops}; Min. crop dimension - {min_crop_dim}\
         \nTotal insect crops - {total_crops}\
         \nGround-truth moth crops - {gt_moths} ({round(gt_moths/total_crops*100,2)}%)\
         \nGround-truth non-moth crops - {gt_nonmoths} ({round(gt_nonmoths/total_crops*100,2)}%)\
@@ -106,7 +105,8 @@ def binary_model_evaluation(
         \nPrecision - {precision}%\
         \nRecall - {recall}%\
         \nF1 score - {fscore}%\
-        \n"
+        \n", 
+        flush=True
     )
 
 if __name__ == "__main__":
@@ -115,10 +115,10 @@ if __name__ == "__main__":
     model_type = "resnet50"
     model_dir = "/home/mila/a/aditya.jain/scratch/eccv2024_data/models/binary"
     category_map = "05-moth-nonmoth_category_map.json"
-    wbds_files = "/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/webdataset/binary_classification/binary-{000000..000011}.tar"
+    insect_crops_dir = "/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/insect_crops"
     skip_small_crops = True
-    min_crop_dim = 100
+    min_crop_dim = 200
 
     binary_model_evaluation(
-        run_name, wandb_model_artifact, model_type, model_dir, category_map, wbds_files
+        run_name, wandb_model_artifact, model_type, model_dir, category_map, insect_crops_dir, skip_small_crops, min_crop_dim
     )
