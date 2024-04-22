bfdf2214be73c562bd9d03c45fa64c2a386a9786
adityajain07 <aditya.jain@mila.quebec>
2024-04-22 14:36:21 -0400

    Completed with all experiments

--- research/eccv2024/model_evaluation/job_fgrained_model_evaluation.sh
+++ research/eccv2024/model_evaluation/job_fgrained_model_evaluation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=fgrained_model_evaluation1
+#SBATCH --job-name=fgrained_model_evaluation
 #SBATCH --partition=long                      # Ask for long job
 #SBATCH --cpus-per-task=2                     # Ask for 2 CPUs
 #SBATCH --gres=gpu:rtx8000:1                  # Ask for 1 GPU
 #SBATCH --mem=10G                             # Ask for 10 GB of RAM
-#SBATCH --output=fgrained_model_evaluation1.out
+#SBATCH --output=fgrained_model_evaluation2.out
 
 # 1. Load the required modules
 module load anaconda/3
@@ -14,21 +14,14 @@ conda activate milamoth_ai
 
 # 3. Launch your script(s)
 python fgrained_model_evaluation.py \
---run_name america_resnet50_baseline_run1 \
---artifact moth-ai/ami-gbif-fine-grained/model:v13 \
---region NorthEasternAmerica \
---model_type resnet50 \
---model_dir /home/mila/a/aditya.jain/scratch/eccv2024_data/models/fine_grained \
---category_map 01_ami-gbif_fine-grained_ne-america_category_map.json \
---insect_crops_dir /home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/insect_crops \
---sp_exclusion_list_file /home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/ami-traps_sp_missing_in_ami-gbif.pickle \
---ami_traps_taxonomy_map_file /home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/ami-traps_taxonomy_map.csv \
---ami_gbif_taxonomy_map_file /home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/ami-gbif_taxonomy_map.csv \
---gbif_taxonomy_hierarchy_file /home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/gbif_taxonomy_hierarchy.json
-
-
-
-
-
-
-
+w-europe_vitb128_baseline_run2 \
+moth-ai/ami-gbif-fine-grained/model:v54 \
+WesternEurope \
+timm_vit-b16-128 \
+/home/mila/a/aditya.jain/scratch/eccv2024_data/models/fine_grained \
+02_ami-gbif_fine-grained_w-europe_category_map.json \
+/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/insect_crops \
+/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/ami-traps_sp_missing_in_ami-gbif.pickle \
+/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/ami-traps_taxonomy_map.csv \
+/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/ami-gbif_taxonomy_map.csv \
+/home/mila/a/aditya.jain/scratch/eccv2024_data/camera_ready_amitraps/metadata/gbif_taxonomy_hierarchy.json
