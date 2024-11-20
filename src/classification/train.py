dc98e9b64b3c708650877bc9a544527258bcdcf9
adityajain07 <aditya.jain@mila.quebec>
2024-11-20 17:14:55 -0500

    Add standalone model inference class

--- src/classification/train.py
+++ src/classification/train.py
@@ -322,32 +322,3 @@ def train_model(
             f"{model_save_path}_checkpoint.pt", type="model", name=wandb_run_name
         )
         wandb.finish()
-
-
-if __name__ == "__main__":
-    train_model(
-        random_seed=42,
-        model_type="resnet50",
-        num_classes=29176,
-        existing_weights=None,
-        total_epochs=10,
-        warmup_epochs=2,
-        early_stopping=5,
-        train_webdataset="/home/mila/a/aditya.jain/scratch/sample_global_model_webdataset/train/train450-{000000..000002}.tar",
-        val_webdataset="/home/mila/a/aditya.jain/scratch/sample_global_model_webdataset/val/val450-000000.tar",
-        test_webdataset="/home/mila/a/aditya.jain/scratch/sample_global_model_webdataset/test/test450-000000.tar",
-        image_input_size=128,
-        batch_size=64,
-        preprocess_mode="torch",
-        optimizer_type="adamw",
-        learning_rate=0.001,
-        learning_rate_scheduler="cosine",
-        weight_decay=1e-5,
-        loss_function_type="cross_entropy",
-        label_smoothing=0.1,
-        mixed_resolution_data_aug=True,
-        model_save_directory="/home/mila/a/aditya.jain/scratch",
-        wandb_entity=None,
-        wandb_project=None,
-        wandb_run_name=None,
-    )
