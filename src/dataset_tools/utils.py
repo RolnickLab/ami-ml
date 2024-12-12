7bfbf9fd63dbab9c733d22a7800b80092a3310f8
adityajain07 <aditya.jain@mila.quebec>
2024-12-12 02:41:59 -0500

    Fetch family and order key from DwC-A

--- src/dataset_tools/utils.py
+++ src/dataset_tools/utils.py
@@ -32,6 +32,8 @@ def load_dwca_data(dwca_file: str):
             "id",
             "datasetKey",
             "speciesKey",
+            "familyKey",
+            "orderKey",
             "acceptedTaxonKey",
             "lifeStage",
             "decimalLatitude",
