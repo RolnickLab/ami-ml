7c52eb86e0dee2e09c6dd8dda2d797cae9182159
adityajain07 <aditya.jain@mila.quebec>
2025-02-12 07:04:19 -0500

    Function barebones

---
+++ research/confidence_calibration/temperature_scaling.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Temperature scaling for confidence calibration"""
+
+
+def expected_calibration_error():
+    """Calculate expected calibration error"""
+    pass
+
+
+def find_scaling_parameter():
+    """Calculate the temperature scaling parameter on the validation dataset"""
+
+
+def calibration_error_on_gbif_test():
+    """Calculate ECE on GBIF test set before and after temperature scaling"""
+    pass
+
+
+def calibration_error_on_ami_traps():
+    """Calculate ECE on AMI-Traps dataset before and after temperature scaling"""
+    pass
+
+
+def reliability_diagram():
+    """Plot reliability diagram"""
+
+
+def main():
+    """ "Main function for confidence calibration"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
