e704a05562e4640b1e1f82775f299d27381e8635
adityajain07 <aditya.jain@mila.quebec>
2025-02-14 07:18:34 -0500

    Tune temp parameter

--- research/confidence_calibration/temperature_scaling.py
+++ research/confidence_calibration/temperature_scaling.py
@@ -3,14 +3,34 @@
 
 """Temperature scaling for confidence calibration"""
 
+import os
+
+import dotenv
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from src.classification.constants import RESNET50
+from src.classification.dataloader import build_webdataset_pipeline
+from src.classification.utils import build_model
+
+dotenv.load_dotenv()
+
+
+class TemperatureScaling(nn.Module):
+    """Temperature scaling class"""
+
+    def __init__(self):
+        super().__init__()
+        self.temperature = nn.Parameter(torch.ones(1))  # Initialize T = 1
+
+    def forward(self, logits):
+        return logits / self.temperature
+
 
 def expected_calibration_error():
     """Calculate expected calibration error"""
     pass
-
-
-def find_scaling_parameter():
-    """Calculate the temperature scaling parameter on the validation dataset"""
 
 
 def calibration_error_on_gbif_test():
@@ -27,10 +47,77 @@ def reliability_diagram():
     """Plot reliability diagram"""
 
 
-def main():
+def tune_temperature(
+    model: torch.nn.Module, val_dataloader: torch.utils.data.DataLoader, device: str
+):
+    """Main temperature tuning function"""
+
+    model.eval()
+    logits_list = []
+    labels_list = []
+    for images, labels in val_dataloader:
+        images, labels = images.to(device), labels.to(device)
+        logits = model(images)
+        logits_list.append(logits)
+        labels_list.append(labels)
+
+    # Concatenate all the logits and labels
+    logits = torch.cat(logits_list)
+    labels = torch.cat(labels_list)
+
+    temp_scaling = TemperatureScaling().to(device)
+    optimizer = optim.LBFGS([temp_scaling.temperature], lr=0.01, max_iter=50)
+
+    def closure():
+        optimizer.zero_grad()
+        loss = nn.CrossEntropyLoss()(temp_scaling(logits), labels)
+        loss.backward(retain_graph=True)
+        return loss
+
+    optimizer.step(closure)
+
+    print(f"Optimal Temperature: {temp_scaling.temperature.item()}")
+    return temp_scaling
+
+
+def main(
+    model_weights: str,
+    model_type: str,
+    num_classes: int,
+    val_webdataset: str,
+    image_input_size: int,
+    batch_size: int,
+    preprocess_mode: str,
+):
     """ "Main function for confidence calibration"""
-    pass
+
+    # Model initialization
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"The available device is {device}.")
+    model = build_model(device, model_type, num_classes, model_weights)
+
+    # Dataloader
+    val_dataloader = build_webdataset_pipeline(
+        val_webdataset,
+        image_input_size,
+        batch_size,
+        preprocess_mode,
+    )
+
+    # Tune temperature parameter
+    _ = tune_temperature(model, val_dataloader, device)
 
 
 if __name__ == "__main__":
-    main()
+    MODEL_WEIGHTS = os.getenv("QUEBEC_VERMONT_WEIGHTS")
+    CONF_CALIB_VAL_WBDS = os.getenv("CONF_CALIB_VAL_WBDS")
+
+    main(
+        model_weights=MODEL_WEIGHTS,
+        model_type=RESNET50,
+        num_classes=2497,
+        val_webdataset=CONF_CALIB_VAL_WBDS,
+        image_input_size=128,
+        batch_size=64,
+        preprocess_mode="torch",
+    )
