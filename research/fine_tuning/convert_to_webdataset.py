43106819b1a5b6bf03b4fad576ef6e3906b85a82
adityajain07 <aditya.jain@mila.quebec>
2025-05-14 16:37:57 -0400

    Convert raw crops to ML structured data

---
+++ research/fine_tuning/convert_to_webdataset.py
@@ -0,0 +1,16 @@
+def convert_to_webdataset(ami_traps_dataset_dir: str, fine_tuning_dataset_dir: str):
+    """Converts a dataset of insect trap images into the WebDataset format for fine-tuning.
+
+    Args:
+        ami_traps_dataset_dir (str): Path to the directory containing the original AMI traps dataset.
+        fine_tuning_dataset_dir (str): Path to the directory where the raw AMI traps data will be stored.
+
+    Returns:
+        None: The function processes and saves the dataset in the specified directory.
+    """
+
+    # Store the data on disk
+
+
+if __name__ == "__main__":
+    pass
